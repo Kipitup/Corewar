63fbf10f68a6f4f95def67409678db0d8ce9dc1b
idinorio <francis.kante@gmail.com>
2020-07-17 22:36:06 +0200

    [VISU] norme header visu

--- include/visu.h
+++ include/visu.h
@@ -6,7 +6,7 @@
 /*   By: francis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 09:28:58 by francis           #+#    #+#             */
-/*   Updated: 2020/07/17 18:09:33 by francis          ###   ########.fr       */
+/*   Updated: 2020/07/17 18:19:47 by francis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,51 +63,50 @@ typedef void	(*t_color_func)(t_window *win, SDL_Rect *rect, char *str);
 /*
 ** ******************************* INIT ****************************************
 */
-void		setup_window(t_vm *vm, t_window *win);
-void		draw_init_zones(t_vm *vm, t_window *win, t_all_rec *all_rec);
-void		init_arena(t_window *win);
+void			setup_window(t_vm *vm, t_window *win);
+void			draw_init_zones(t_vm *vm, t_window *win, t_all_rec *all_rec);
+void			init_arena(t_window *win);
 
 /*
 ** ****************************** RUNNING **************************************
 */
-void		active_zones(t_vm *vm, t_window *win, t_all_rec *all_rec);
-void		active_arena(t_vm *vm, t_window *win);
-int			run_visu(t_vm *vm, t_window *win);
+void			active_zones(t_vm *vm, t_window *win, t_all_rec *all_rec);
+void			active_arena(t_vm *vm, t_window *win);
+int				run_visu(t_vm *vm, t_window *win);
 /*
 ** ******************************* CLEAN ***************************************
 */
-void		destroy_visual(t_window *win);
+void			destroy_visual(t_window *win);
 
 /*
 ** ******************************* TOOLS ***************************************
 */
-SDL_Rect	init_new_rect(int x, int y, int width, int height);
-SDL_Point	set_point(int x, int y);
-SDL_Color	set_color(int red, int green, int blue, int alpha);
-void		event_handler(t_window *win);
+SDL_Rect		init_new_rect(int x, int y, int width, int height);
+SDL_Point		set_point(int x, int y);
+SDL_Color		set_color(int red, int green, int blue, int alpha);
+void			event_handler(t_window *win);
 
 /*
 ** ****************************** WRITING **************************************
 */
-void		classic_writing(t_window *win, SDL_Rect *rect, char *str);
-void		green_writing(t_window *win, SDL_Rect *rect, char *str);
-void		blue_writing(t_window *win, SDL_Rect *rect, char *str);
-void		red_writing(t_window *win, SDL_Rect *rect, char *str);
-void		purple_writing(t_window *win, SDL_Rect *rect, char *str);
+void			classic_writing(t_window *win, SDL_Rect *rect, char *str);
+void			green_writing(t_window *win, SDL_Rect *rect, char *str);
+void			blue_writing(t_window *win, SDL_Rect *rect, char *str);
+void			red_writing(t_window *win, SDL_Rect *rect, char *str);
+void			purple_writing(t_window *win, SDL_Rect *rect, char *str);
 
 /*
 ** ****************************** DRAWING **************************************
 */
-void		player_box(t_vm *vm, t_window *win, t_all_rec *rec, int x, int y);
-void		info_box(t_window *win, t_all_rec *rec, int x, int y);
-void		command_box(t_window *win, t_all_rec *rec,int x, int y);
-void		draw_arena(t_window *win);
-void		draw_header_rec(t_window *win);
-void		draw_player_info_comm_rec(t_vm *vm, t_window *win, t_all_rec *rec);
-
-void		player1_input_writing(t_window *win, t_all_rec *rec, int x, int y);
-void		player2_input_writing(t_window *win, t_all_rec *rec, int x, int y);
-void		player3_input_writing(t_window *win, t_all_rec *rec, int x, int y);
-void		player4_input_writing(t_window *win, t_all_rec *rec, int x, int y);
+void			player_box(t_vm *vm, t_window *win, t_all_rec *rec, int x, int y);
+void			info_box(t_window *win, t_all_rec *rec, int x, int y);
+void			command_box(t_window *win, t_all_rec *rec,int x, int y);
+void			draw_arena(t_window *win);
+void			draw_header_rec(t_window *win);
+void			draw_player_info_comm_rec(t_vm *vm, t_window *win, t_all_rec *rec);
+void			player1_input_writing(t_window *win, t_all_rec *rec, int x, int y);
+void			player2_input_writing(t_window *win, t_all_rec *rec, int x, int y);
+void			player3_input_writing(t_window *win, t_all_rec *rec, int x, int y);
+void			player4_input_writing(t_window *win, t_all_rec *rec, int x, int y);
 
 #endif
